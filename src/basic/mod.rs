f60112627d33f6e0631ca08f5d7d2091a50b5ef1
misrab <faizullah.misrab@gmail.com>
2017-11-13 16:51:57 +0800

    commit

--- src/basic/mod.rs
+++ src/basic/mod.rs
@@ -1,9 +1,36 @@
+use std::fs;
+use std::io::prelude::*;
+
+
+static DB_FILE: &'static str = "db.txt";
 
 
 
+// open db connection
+pub fn open_connection() {
+  let mut file = fs::File::create(DB_FILE);
+}
+
+// add the string to the file
+pub fn db_set(s: &str) {
+  let mut file = fs::File::open(DB_FILE).unwrap();
+
+  if let Err(e) = writeln!(file, "{}", s) {
+    println!("{}", e);
+  }
+}
+
+// clear contents
+pub fn delete_connection() {
+  fs::remove_file(DB_FILE);
+}
 
 
+/*
 #[test]
 fn test_basic() {
   println!("test basic");
-}
+
+
+  open_connection();
+}*/
--- src/tests.rs
+++ src/tests.rs
@@ -13,4 +13,16 @@ mod test {
   fn test_test() {
     println!("tests are running");
   }
+
+
+
+  #[test]
+  fn test_basic() {
+    println!("test basic");
+
+    basic::open_connection();
+    basic::db_set("mooo");
+    basic::delete_connection();
+  }
+
 }
